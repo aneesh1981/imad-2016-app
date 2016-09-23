6cd8970b723ec2895c903a46f3f724f762da60e9
aneesh1981 <aneeshchandran1981@gmail.com>
2016-09-23 05:53:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/ui/article-1.html',function(req,res)
+{res.sendFile("request will be served1");
+});
+app.get('/ui/article-2.html',function(res,req)
+{res.sendFile("requested will be served 2");});
+
+app.get('/ui/article-3.html',function(req,res)
+{res.sendFile("request will be served3");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
