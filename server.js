dba97950636315c706d51f067169dabef257ccf2
aneesh1981 <aneeshchandran1981@gmail.com>
2016-09-26 05:34:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res) {
-  res.send("request will be served1");
+   res.sendFile(path.join(__dirname, 'ui', 'Article-one'));
 });
 
 app.get('/article-two',function(req, res){
