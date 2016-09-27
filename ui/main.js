5ff4d2abb0c2863fbf7fcb8788554366aae1fc55
aneesh1981 <aneeshchandran1981@gmail.com>
2016-09-27 19:27:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,13 @@ console.log(element);
 // Moving madi
 
 var move = document.getElementById('madi');
+
+var moveRight = function()
+{
+var marginLeft=marginLeft+10;
+move.style.marginLeft=marginLeft+'px';
+};
 move.onclick= function(){
-    move.style.marginLeft='100px';
+    var interval = setInterval(moveRight,20);
+    //move.style.marginLeft='100px';
 };
