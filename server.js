faa9a87a706c1369cd6e02f6633d00cfbe6fbeef
aneesh1981 <aneeshchandran1981@gmail.com>
2016-09-27 06:26:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ app.get('/article-3',function(req, res){
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
