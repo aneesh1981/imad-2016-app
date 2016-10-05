bfebf3b208fb8f6d339bb803290b099ba37d8d27
aneesh1981 <aneeshchandran1981@gmail.com>
2016-10-06 05:17:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ var counter=0;
 button.onclick = function(){
     counter=counter+1;
     var span= document.getElementById('count');
+    consol.log(span);
     span.InnerHtml=counter.toString();
     
 };
