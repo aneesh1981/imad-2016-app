4aa20bdca479c2146bb159ffde97b8a2762452f5
aneesh1981 <aneeshchandran1981@gmail.com>
2016-10-06 21:26:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,25 @@ var button  = document.getElementById('counter') ;
 var counter=0;
 
 button.onclick = function(){
-    counter=counter+1;
+    //makerequest
+    var request =new XMLHttpRequest();
+    
+    //capture request
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttpRequest.done)
+        {//some action
+            if (request.status===200){
+                var counter = request.responseText;
+                 var span= document.getElementById('count');
+                span.innerHTML=counter.toString();
+                }
+        }
+        
+    };
+    
+    //counter=counter+1;
    // console.log(counter);
-    var span= document.getElementById('count');
-    span.innerHTML=counter.toString();
-    console.log(span);
+   request.open('GET','http://aneesh1981.imad.hasura.io/counter',true);
+   request.send(null);
+   
 };
